62fb8e2e7f4244abbc219c4e330c4a3a08a76541
Suhani Sharma <suhani190505@gmail.com>
2025-10-22 17:48:30 +0530

    intial commit

---
+++ project2/card/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Card() {
+  return (
+    <div className="Card">
+    <img src="/src/assets/profile.png" alt="" />
+     <h1>I'm Suhani!</h1>
+     <h1>I live.</h1>
+     <h1>WEB DEVELOPER</h1>
+     <h1>INFLUENCER</h1>
+    </div>
+
+  )
+}
+
+export default Card
